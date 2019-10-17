066a41cfd4558bbe0ea9bf084279d8aa12395bcf
Karim Ibrahim <40270714+chillman12@users.noreply.github.com>
2019-10-17 16:21:25 -0400

    Add project files.

---
+++ MovieRatings/Controllers/RatingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRatingsDB;
+
+namespace MovieRatings.Controllers
+{
+    public class RatingController : Controller
+    {
+        // GET: Rating
+        public ActionResult Index()
+        {
+            var cust = DB.GetMovies();
+            ViewBag.Movies = cust;
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            //get model from database
+            ViewBag.Movie = DB.GetRating(id);
+            return View();
+        }
+    }
+}
